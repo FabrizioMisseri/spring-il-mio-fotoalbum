eff65fc9c918c5c028e62fc0f092e851c520af61
FabrizioMisseri <misserifabrizio@gmail.com>
2023-04-13 21:41:58 +0200

    add: CategoryNotFoundException

---
+++ src/main/java/org/learning/java/springilmiofotoalbum/exceptions/CategoryNotFoundException.java
@@ -0,0 +1,8 @@
+package org.learning.java.springilmiofotoalbum.exceptions;
+
+public class CategoryNotFoundException extends RuntimeException {
+
+    public CategoryNotFoundException(String message) {
+        super(message);
+    }
+}
