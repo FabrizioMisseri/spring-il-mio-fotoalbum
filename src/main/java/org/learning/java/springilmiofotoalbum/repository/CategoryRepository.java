05dbd85df393ccff9b9af4c866d8fc8b50b63d4c
FabrizioMisseri <misserifabrizio@gmail.com>
2023-04-13 12:17:54 +0200

    add repositories

---
+++ src/main/java/org/learning/java/springilmiofotoalbum/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package org.learning.java.springilmiofotoalbum.repository;
+
+import org.learning.java.springilmiofotoalbum.model.Category;
+import org.learning.java.springilmiofotoalbum.model.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+}
+
---
+++ src/main/java/org/learning/java/springilmiofotoalbum/repository/PhotoRepository.java
@@ -0,0 +1,11 @@
+package org.learning.java.springilmiofotoalbum.repository;
+
+import org.learning.java.springilmiofotoalbum.model.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PhotoRepository extends JpaRepository<Photo, Integer> {
+    public List<Photo> findByTitleContainingIgnoreCase(String name);
+}
+
